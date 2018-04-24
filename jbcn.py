30009dbd1562ebf25ee1b5863fe24d0a28629e73
Jun Furuse <jun.furuse@asahinet.com>
2018-04-24 01:19:20 +0000

    Added some more comments. Headless mode (commented out)

--- jbcn.py
+++ jbcn.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 import time
-from selenium import webdriver
 from sys import argv
 import re
 from pathlib import Path
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 home = str(Path.home())
 
@@ -19,6 +20,7 @@ chromedriver_path= home + '/Downloads/chromedriver/chromedriver'
 #
 # * The email+password file.  It must have only one line of <email>:<password>.
 #   Drop the accessibility form the others.
+#   Example: 1009999:mypassword
 user_path= home + '/.jbcn'
 #
 ##############################################################################
@@ -30,16 +32,16 @@ lines = f.readlines()
 f.close()
 
 if len(lines) != 1:
-    print("Error: user_path file must contain only one line")
+    print("Error: password file must contain only one line")
+    exit(1)
 
 m = re.match("([^:]+):([^:\n\r]+)[\r\n]+", lines[0])
 if not m:
-    print("Error: user_path format error")
+    print("Error: password format error")
+    exit(1)
 
-email=m.group(1)
+email=m.group(1)    # It is called "email" in the HTML.
 password=m.group(2)
-
-# print("email=%s password=%s.\n" % (email, password))
 
 mode=str(argv[1])
 
@@ -50,11 +52,19 @@ ids = {
 
 id = ids[mode]
 
-driver = webdriver.Chrome(chromedriver_path)  # Optional argument, if not specified will search path.
+if not id:
+    print("jbcn [start|end]")
+    exit(1)
+
+chrome_options = Options()
+# chrome_options.add_argument("--headless")
+driver = webdriver.Chrome(chromedriver_path, chrome_options=chrome_options)
 
 driver.get('https://ssl.jobcan.jp/employee');
 
 time.sleep(5) # Let the user actually see something!
+
+# Fill the form, then submit it
 
 e = driver.find_element_by_id("client_id")
 e.send_keys("asahinet")
@@ -69,6 +79,7 @@ e.submit()
 
 time.sleep(5) # Let the user actually see something!
 
+# click start or end
 e = driver.find_element_by_id(id)
 e.click()    
     
